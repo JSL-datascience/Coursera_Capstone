8c80c7cc7eebb6bb9184e9fbc4774cf079461564
JasonLeung <sze-lok@hotmail.com>
2020-02-22 17:06:13 +1100

    importing pandas and numpy and printing hello capstone project for Data Science Course

---
+++ python_code.py
@@ -0,0 +1,9 @@
+"Hello. This notebook will be mainly used for capstone project "
+"Import pandas and numpy"
+import pandas as pd
+import numpy as np
+
+"Print Hello Capstone Project Course"
+print('Hello Capstone Project Course')
+
+
